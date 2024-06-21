9918dfe9e95056858c483578efa2c510ebb7e95a
jmt <148846218+jmtae@users.noreply.github.com>
2024-06-21 15:29:37 +0900

    Update 한국정책브리핑_크롤링.py 최종본
    주석처리와 제이슨 문제 해결

--- 한국정책브리핑_크롤링.py
+++ 한국정책브리핑_크롤링.py
@@ -17,88 +17,90 @@
 # result = [e.text.strip() for e in soup.select("a") if e.text.strip() != '']
 # print(result)
 
-#Part 1. 모듈 가져오기
+#[1. 라이브러리 모듈 불러오기]
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
-import pandas as pd
-import datetime
-import math
-from time import sleep
-import warnings
-import requests
-# 데이터 저장을 위한 json parsing
 import json
+import datetime
+from time import sleep
+import math
+import requests
+import pandas as pd
+# 경고 표시 무시하기.
+import warnings
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
-#Part 2. 키워드 수집 설정
-Keyword = input('1. 수집 검색어는 무엇입니까?: ')
-ft_name = "articles"
-# fx_name = input('3. 결과를 저장할 xlsx형식의 파일명을 쓰세요(예: 파일명.xlsx): ')       
-# [c:\\py_temp\\파일명.xlsx] 절대경로 지정방법
+# [2. 정책 검색하기]
+# 정책 중 키워드를 골라서 검색하기
+print('='* 100)
+Search_text = input('안녕하세요 한국 정책 브리핑중  어떤 관련 정책이 궁금하시나요?: ')
 
-
-#Part 3. 셀레니움 & BS4 크롬 제어
+# [3. 셀리니움 불러오기] 
 options = webdriver.ChromeOptions()
 options.add_argument('start-maximized')
-##driver = webdriver.Chrome('/Users/kaz/Desktop/Project/chromedriver', options=options)
 
-# 브라우저 자동 꺼짐 방지 옵션
+
+# Selenium 스크립트가 종료된 후에도 브라우저가 자동으로 닫히지 않도록 설정
 chrome_options = Options()
 chrome_options.add_experimental_option("detach",True)
-# 크롬드라이버 생성
+
+# 크롬드라이버 설정
 driver = webdriver.Chrome(options=chrome_options)
-# 페이지 로딩이 완료될 때까지 기다리는 코드
+
+#대기 시간으로 WebDriver가 요소를 찾을 때까지 대기
 driver.implicitly_wait(5)
+# 타겟 url은 한국 정책 브리핑 사이트.
 TARGET_URL = 'https://www.korea.kr/news/pressReleaseList.do'
+
+# 드라이버로 url열기
 driver.get(TARGET_URL)
 
+# 키워드 열고 검색창 클릭
 driver.find_element(By.ID, 'srchKeyword').click()
-driver.find_element(By.ID, 'srchKeyword').send_keys(Keyword + Keys.ENTER)
-sleep(1)
+driver.find_element(By.ID, 'srchKeyword').send_keys(Search_text + Keys.ENTER)
+sleep(0.5)
 
-driver.find_element(By.XPATH, '//*[@id="period"]/option[2]').click()        #최근 1주 검색 <2번쨰.>
+# 검색 날 지정하기. 1달이나 1년은 너무 많으니 최근 이슈로 지정 <2번째> 셀렉터가 안 찾아와져 XPATH로 가지고 오기.
+driver.find_element(By.XPATH, '//*[@id="period"]/option[2]').click()        
 sleep(1)
 driver.find_element(By.XPATH, '//*[@id="container"]/div/div/div/div[2]/div[1]/button/span').click() 
 
-# driver.find_element(By.XPATH, '//*[@id="container"]/div/article/div[1]/div[2]/div[1]/button/span').click() 원래 있던 코드
-
-
+# 파싱하기
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
-Result_Articles = int(soup.select_one('div.result > strong').text)	#검색 결과 총 000건 
-print("총 검색 결과 수는", Result_Articles)
-ArticlePages = math.ceil(int(Result_Articles) / 20)	# 브리핑룸은 20개 단위로 늘어남 그래서 20개를 나누어서 페이지를 넘기는 거임.
-PageNum = 1
+# 검색 결과 총 수 가져오기.
+Total_Article_n = int(soup.select_one('div.result > strong').text)	
+print("브리핑 총 검색 결과 수는", Total_Article_n,"개 입니다.")
+# 브리핑 페이지수가 20개 단위로 늘어나기에 20개를 나누어서 총 넘겨야 할 페이지 지정.
+Pages_Article = math.ceil(int(Total_Article_n) / 20)	
+Pagenum = 1
 
-print('=' *80)
+print('=' * 100)
     
-print('3초 후에 시작합니다.')
+print('<3초 크롤링 시작>')
 sleep(3)
 
-'''
-#Part 4. 각 항목별 데이터를 수집하여 리스트에 저장
-sn2 = [ ]       #연번 저장
-date2 = [ ]     #날짜 저장
-'''
-total = [ ]      #링크 저장
+
+# [4. 크롤링 저장 변수 지정]
+total_list = [ ]      # 전체 정책들 저장 변수
 title1 = [ ]    #기사제목 저장
-ogz1 = [ ]      #부처 저장
-no = 1
+organiz1 = [ ]      #부속 기관들 (부처)) 저장
+no = 1 # 정책들 넘기기
 
 req = requests.get(TARGET_URL)
 html_3 = req.text
 
-
-for a in range(1, ArticlePages + 1) :
+# 크롤링 시작
+for x in range(1, Pages_Article + 1) : # 총 페이지 
     
     i=1
     for i in range(1,21):
-        if no > Result_Articles:
+        if no > Total_Article_n: # 정책 수가 통합 수보다 크면 빠져나오기.
             break
         
         html_3 = driver.page_source 
@@ -106,7 +108,7 @@ for a in range(1, ArticlePages + 1) :
         
         # 정책 제목 타이틀 가져오기
         content_3 = soup_3.select(f'div.list_type > ul > li:nth-child({i}) > a > span > strong')
-        # #container > div > div > div > div.list_type > ul > li:nth-child(1) > a > span > strong
+        
         print("\n")
         print(f"--{no}번--")
         for title in content_3:
@@ -114,43 +116,50 @@ for a in range(1, ArticlePages + 1) :
             title1.append(title.get_text(strip=True))
         
         # 부처 가져오기
-        # 셀럭터 container > div > div > div > div.list_type > ul > li:nth-child(1) > a > span > span.source > span:nth-child(2)
+        
         content_4 = soup_3.select(f'div.list_type > ul > li:nth-child({i}) > a > span > span.source > span:nth-child(2)')
         for department in content_4:
             print("부처:", department.get_text(strip = True))
-            ogz1.append(department.get_text(strip=True))
+            organiz1.append(department.get_text(strip=True))
+            
+        for title, department in zip(content_3, content_4):
+            total_list.append({
+                "Title": title.get_text(strip=True),
+                "Organization": department.get_text(strip=True)
+            })
+    
             
         no += 1
-        
-        for titles, ogzs in zip(title,department):
-            total.append({
-                "Title": titles,
-                "Organization":ogzs
-            })
             
-            
-                
-    if no > Result_Articles:
+    if no > Total_Article_n:
         break
-    a += 1 
-    b = str(a)
+    
+    x += 1 
+    y = str(x)
     
     # 페이지 바꿈 함수
-    driver.find_element(By.LINK_TEXT ,'%s' %b).click()
-    print(f'1초 후에 {b}페이지 크롤링 시작합니다.')
+    driver.find_element(By.LINK_TEXT ,'%s' %y).click()
+    print(f'1초 후에 {y}페이지 크롤링 시작합니다.')
     driver.implicitly_wait(5)
     sleep(1)
 
-# 크롤링한 데이터를 데이터프레임으로 저장
+#[5. 크롤링 데이터 저장]
+# 크롤링한 데이터를 데이터프레임으로 저장(제이슨으로 저장할거지만 프레임으로도 보기위하여)
 df = pd.DataFrame({
     '기사제목': title1,
-    '부처': ogz1,
+    '부처': organiz1,
 })
 
-with open("한국정책브리핑.jsonl", "w", encoding="UTF-8") as output_file:
-    for articles in total:
-        print(json.dumps(articles, ensure_ascii=False), file=output_file)
+#[6. json으로 저장]
+with open("korea_briefing.jsonl", "w", encoding="UTF-8") as output_file:
+    for article in total_list:
+        print(json.dumps(article, ensure_ascii=False), file=output_file)
 
 #######################################################
 
 
+# # JSONL 파일로 저장
+# with open("한국정책브리핑.jsonl", "w", encoding="UTF-8") as output_file:
+#     for article in total:
+#         json_line = json.dumps(article, ensure_ascii=False)
+#         output_file.write(json_line + "\n")
